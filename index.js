ad5da6504af988ec35a34ea5f5ead5db65b8e328
Burak Karakan <burak.karakan@gmail.com>
2019-12-04 14:46:42 +0100

    Removed try-catch to have a better error visibility.

--- index.js
+++ index.js
@@ -4,47 +4,42 @@ function parseGh(gitHubHtml) {
   var $ = cheerio.load(gitHubHtml)
   var items = []
   $('article.Box-row').each(function (i) {
-    try {
-      var item = $(this);
-      var divs = item.children('div');
-      var h1 = item.children('h1').first();
-      var repo = getRepoInformation(item.children('h1').first(), item.children('p').first());
+    var item = $(this);
+    var divs = item.children('div');
+    var h1 = item.children('h1').first();
+    var repo = getRepoInformation(item.children('h1').first(), item.children('p').first());
 
-      var metaRow = divs.last();
-      var metaLinks = metaRow.children('a');
+    var metaRow = divs.last();
+    var metaLinks = metaRow.children('a');
 
-      var stars = toNumeric(metaLinks.first());
-      var forks = toNumeric(metaLinks.eq(1));
-      var todayStars = parseInt(metaRow.children('span').last().text().trim().split(' ')[0].replace(/,/g, ""));
+    var stars = toNumeric(metaLinks.first());
+    var forks = toNumeric(metaLinks.eq(1));
+    var todayStars = parseInt(metaRow.children('span').last().text().trim().split(' ')[0].replace(/,/g, ""));
 
-      var language = metaRow.find('[itemprop="programmingLanguage"]').first();
-      var languageFound = language && language.text().trim() !== '';
-      var languageColor = languageFound ? language.siblings('.repo-language-color').first() : null;
+    var language = metaRow.find('[itemprop="programmingLanguage"]').first();
+    var languageFound = language && language.text().trim() !== '';
+    var languageColor = languageFound ? language.siblings('.repo-language-color').first() : null;
 
-      var resultObject = {
-        repo,
-        stars: {
-          count: stars,
-          link: metaLinks.first().attr('href').trim()
-        },
-        forks: {
-          count: isNaN(forks) ? 0 : forks,
-          link: metaLinks.eq(1).attr('href') ? metaLinks.eq(1).attr('href').trim() : ''
-        },
-        todayStars,
-        ...(languageFound && {
-          language: {
-            is: language && language.text().trim() !== '' ? language.text().trim() : null,
-            color: languageColor ? languageColor.css('background-color') : null,
-          }
-        }),
-      };
+    var resultObject = {
+      repo,
+      stars: {
+        count: stars,
+        link: metaLinks.first().attr('href').trim()
+      },
+      forks: {
+        count: isNaN(forks) ? 0 : forks,
+        link: metaLinks.eq(1).attr('href') ? metaLinks.eq(1).attr('href').trim() : ''
+      },
+      todayStars,
+      ...(languageFound && {
+        language: {
+          is: language && language.text().trim() !== '' ? language.text().trim() : null,
+          color: languageColor ? languageColor.css('background-color') : null,
+        }
+      }),
+    };
 
-      items[i] = resultObject;
-    } catch (error) {
-      console.error('repo', repo)
-      console.error(error)
-    }
+    items[i] = resultObject;
   });
 
   return items;
